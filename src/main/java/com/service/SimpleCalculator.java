a74fd8620fca1e0aac6ced0bd3a38f3b1b4385d2
Daan van Driel <daanvandriel@Daans-MacBook-Pro.local>
2020-12-05 09:18:17 +0100

    create SimpleCalculator and method stubs

---
+++ src/main/java/com/service/SimpleCalculator.java
@@ -0,0 +1,20 @@
+package com.service;
+
+public class SimpleCalculator {
+
+    public double add(int leftHand, int rightHand) {
+        return 0.0d;
+    }
+
+    public double subtract(int leftHand, int rightHand) {
+        return 0.0d;
+    }
+
+    public double multiply(int leftHand, int rightHand) {
+        return 0.0d;
+    }
+
+    public double divide(int leftHand, int rightHand) {
+        return 0.0d;
+    }
+}
