afc33f84d685f68d5059bf73128e7181de429977
Daan van Driel <daanvandriel@Daans-MacBook-Pro.local>
2020-12-05 09:51:44 +0100

    add result dto

---
+++ src/main/java/com/rest/dto/SimpleCalculationResultDto.java
@@ -0,0 +1,10 @@
+package com.rest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SimpleCalculationResultDto {
+    private double result;
+}
