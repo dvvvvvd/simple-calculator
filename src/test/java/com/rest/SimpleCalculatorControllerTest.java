61e5ac38d9bea4a1018de3bef8d97f517cf9fc0a
Daan van Driel <daanvandriel@Daans-MacBook-Pro.local>
2020-12-05 10:46:30 +0100

    initial setup for test class

--- src/test/java/com/rest/SimpleCalculatorControllerTest.java
+++ src/test/java/com/rest/SimpleCalculatorControllerTest.java
@@ -1,4 +1,29 @@
 package com.rest;
 
+import com.service.SimpleCalculator;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@RunWith(MockitoJUnitRunner.class)
 public class SimpleCalculatorControllerTest {
+
+    @Mock
+    private SimpleCalculator simpleCalculator;
+
+    @InjectMocks
+    private SimpleCalculationController simpleCalculationController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(simpleCalculationController).build();
+    }
 }
